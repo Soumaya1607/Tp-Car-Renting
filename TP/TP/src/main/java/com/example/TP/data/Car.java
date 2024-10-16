860cb2cadef108edecd50304b605d6cfc7cb65a1
Soumaya1607 <121567390+Soumaya1607@users.noreply.github.com>
2024-10-16 12:41:12 +0200

    Add files via upload

---
+++ TP/TP/src/main/java/com/example/TP/data/Car.java
@@ -0,0 +1,40 @@
+package com.example.TP.data;
+public class Car {
+
+    String plateNumber;
+    int price;
+    boolean rented;
+
+    public Car(String plateNumber, int price) {
+        this.plateNumber = plateNumber;
+        this.price = price;
+        this.rented = false;
+    }
+
+    public Car() {
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    public void setRented(boolean rented) {
+        this.rented = rented;
+    }
+}
